a275cf33eb1f62842ff9e8302f33d14b253b36aa
Romeo Yofe <mrborder3960@gmail.com>
2020-09-12 11:47:00 +0300

    Reworked Response into an interface and added a base implementation

--- src/main/java/me/border/utilities/utils/ImmuteableResponse.java
+++ src/main/java/me/border/utilities/utils/ImmuteableResponse.java
@@ -3,6 +3,7 @@ package me.border.utilities.utils;
 public class ImmuteableResponse<T> implements Response<T> {
 
     private boolean answer;
+    private boolean hasContext = false;
     private T context;
 
     public ImmuteableResponse(boolean answer){
@@ -11,12 +12,20 @@ public class ImmuteableResponse<T> implements Response<T> {
 
     public ImmuteableResponse(boolean answer, T context){
         this(answer);
+        if (context == null)
+            throw new NullPointerException("Response cannot have null context");
+        hasContext = true;
         this.context = context;
     }
 
     @Override
     public boolean getAnswer() {
         return answer;
+    }
+
+    @Override
+    public boolean hasContext() {
+        return hasContext;
     }
 
     @Override
--- src/main/java/me/border/utilities/utils/Response.java
+++ src/main/java/me/border/utilities/utils/Response.java
@@ -2,7 +2,24 @@ package me.border.utilities.utils;
 
 public interface Response<T> {
 
+    /**
+     * Get the answer attached to the response
+     *
+     * @return The answer
+     */
     boolean getAnswer();
 
+    /**
+     * Get if the response has a context
+     *
+     * @return Whether it has a response
+     */
+    boolean hasContext();
+
+    /**
+     * Get the response's context
+     *
+     * @return The context
+     */
     T getContext();
 }
