ae18dc532556dda478695061e217aa55c5535194
BuildTools <unconfigured@null.spigotmc.org>
2020-10-12 08:06:03 +0300

    Added documentation to FileListener and added an ExampleFileListener

---
+++ src/main/java/me/border/utilities/example/ExampleFileListener.java
@@ -0,0 +1,31 @@
+package me.border.utilities.example;
+
+import me.border.utilities.file.watcher.FileEvent;
+import me.border.utilities.file.watcher.FileListener;
+import me.border.utilities.file.watcher.FileWatcher;
+
+import java.io.File;
+
+public class ExampleFileListener {
+
+    public ExampleFileListener(File dir){
+        FileWatcher watcher = new FileWatcher(dir);
+        watcher.addListener(new FileListener() {
+            @Override
+            public void onCreated(FileEvent event) {
+                File file = event.getFile();
+            }
+
+            @Override
+            public void onModified(FileEvent event) {
+                File file = event.getFile();
+            }
+
+            @Override
+            public void onDeleted(FileEvent event) {
+                File file = event.getFile();
+            }
+        });
+        watcher.watch();
+    }
+}
--- src/main/java/me/border/utilities/file/watcher/FileListener.java
+++ src/main/java/me/border/utilities/file/watcher/FileListener.java
@@ -4,9 +4,24 @@ import java.util.EventListener;
 
 public interface FileListener extends EventListener {
 
+    /**
+     * Fired upon creation of a file in the watched directory.
+     *
+     * @param event Event object.
+     */
     void onCreated(FileEvent event);
 
+    /**
+     * Fired upon modification of a file in the watched directory.
+     *
+     * @param event Event object.
+     */
     void onModified(FileEvent event);
 
+    /**
+     * Fired upon deletion of a file in the watched directory.
+     *
+     * @param event Event object.
+     */
     void onDeleted(FileEvent event);
 }
