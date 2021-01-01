8aa62d50b58e84f23c5b117cb5532a2792a948a3
Romeo <mrborder3960@gmail.com>
2021-01-01 17:04:07 +0200

    Renamed ICache and Cache

--- src/main/java/me/border/utilities/cache/Cache.java
+++ src/main/java/me/border/utilities/cache/Cache.java
@@ -1,91 +1,38 @@
 package me.border.utilities.cache;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import me.border.utilities.terminable.Terminable;
 
-public class Cache<K> implements ICache<K> {
-    private final Map<K, Cacheable> cacheHashMap = new HashMap<>();
-    private Thread cleanUpThread;
+public interface Cache<K> extends Terminable {
 
-    private volatile boolean closed = false;
+    /**
+     * Cache a {@link Cacheable}
+     *
+     * @param key Object identifier
+     * @param value The {@link Cacheable} to cache
+     */
+    void cache(K key, Cacheable value);
 
-    public Cache(){
-        this(30, TimeUnit.SECONDS);
-    }
+    /**
+     * Get a {@link Cacheable} from the cache with the corresponding identifier
+     *
+     * @param key The identifier
+     * @return The {@link Cacheable}
+     */
+    Cacheable get(K key);
 
-    public Cache(long sleepTime, TimeUnit tu){
-        try {
-            cleanUpThread = new Thread(() -> {
-                long sleepTimeInMillis = tu.toMillis(sleepTime);
-                try {
-                    while (!closed) {
-                        Set<K> keySet = cacheHashMap.keySet();
-                        for (K key : keySet) {
-                            Cacheable value = cacheHashMap.get(key);
-                            if (value.isExpired()) {
-                                cacheHashMap.remove(key);
-                            }
-                        }
-                        Thread.sleep(sleepTimeInMillis);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-            cleanUpThread.setDaemon(true);
-            cleanUpThread.setPriority(Thread.MIN_PRIORITY);
-            cleanUpThread.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    /**
+     * Get a parsed object from the cache with the corresponding identifier
+     * @see #get(K)
+     *
+     * @param key the identifier
+     * @return The parsed object
+     */
+    Object getParsed(K key);
 
-    @Override
-    public void cache(K key, Cacheable value) {
-        validate();
-        cacheHashMap.put(key, value);
-    }
-
-    @Override
-    public Cacheable get(K key) {
-        validate();
-        Cacheable object = cacheHashMap.get(key);
-        if (object == null)
-            return null;
-        if (object.isExpired()) {
-            cacheHashMap.remove(key);
-            return null;
-        } else {
-            return object;
-        }
-    }
-
-    @Override
-    public Object getParsed(K key) {
-        Cacheable cacheable = get(key);
-        if (cacheable == null)
-            return null;
-        return cacheable.getObject();
-    }
-
-    @Override
-    public void remove(K key) {
-        validate();
-        cacheHashMap.remove(key);
-    }
-
-    @Override
-    public void close() throws Exception {
-        validate();
-        cleanUpThread.interrupt();
-        cacheHashMap.clear();
-        closed = true;
-    }
-
-    @Override
-    public boolean isClosed() {
-        return closed;
-    }
+    /**
+     * Remove an object from the cache
+     *
+     * @param key The object identifier
+     */
+    void remove(K key);
 }
